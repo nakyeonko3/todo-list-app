b1b2fe115398fd9adf3eb86fb2ead0fed060e59c
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-04-10 16:05:49 +0900

    useUpdateTodo에서 onSuccess 로직을 제거
    - useUpdateTodo에서 onSuccess 이후 쿼리키를 변경하면서 생기는 UI 흔들림 현상을 방지하기 위해 onSuccess 로직을 제거했습니다.

--- src/app/hooks/useUpdateTodo.ts
+++ src/app/hooks/useUpdateTodo.ts
@@ -1,5 +1,5 @@
 import { TodoItemSummary, updateTodo } from "@/app/api/api";
-import { TODO_DETAIL_QUERY_KEY, TODOS_QUERY_KEY } from "@/app/constants";
+import { TODOS_QUERY_KEY } from "@/app/constants";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function useUpdateTodo() {
@@ -26,24 +26,6 @@ export default function useUpdateTodo() {
     },
     onError: (_err, _newTodo, context) => {
       queryClient.setQueryData([TODOS_QUERY_KEY], context?.previousTodos);
-    },
-    onSuccess: (data) => {
-      queryClient.invalidateQueries({
-        queryKey: [TODO_DETAIL_QUERY_KEY, data.id],
-      });
-      queryClient.setQueryData([TODO_DETAIL_QUERY_KEY, data.id], data);
-      queryClient.setQueryData([TODOS_QUERY_KEY], (old: TodoItemSummary[]) => {
-        if (!old) return [];
-        return old.map((todo) =>
-          todo.id === data.id
-            ? {
-                id: todo.id,
-                name: data.name ?? todo.name,
-                isCompleted: data.isCompleted ?? todo.isCompleted,
-              }
-            : todo
-        );
-      });
     },
   });
   return mutate;
