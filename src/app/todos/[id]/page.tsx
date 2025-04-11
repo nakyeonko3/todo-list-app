5fdfd0b4e5553e2492363c0968f8d42b0b2e7f59
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-04-11 16:54:12 +0900

    todo 상세 페이지에 ErrorBoundary 적용

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,20 +1,15 @@
 import TodoDetailContent from "@/app/components/TodoDetailContent";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-export default async function TodoDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default function TodoDetailPage({ params }: { params: { id: string } }) {
   const { id } = params;
 
-  //TODO: todoId가 number가 아닐 때 에 대한 예외처리 필요
-
   return (
-    <div className="">
+    <ErrorBoundary fallback={<div>에러 발생</div>}>
       <Suspense fallback={<div>Loading...</div>}>
         <TodoDetailContent itemId={id} />
       </Suspense>
-    </div>
+    </ErrorBoundary>
   );
 }
