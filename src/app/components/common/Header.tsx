51e7094ac647b169c5a739e8d511f739e3694ecd
Ko Nak-yean <59277499+nakyeonko3@users.noreply.github.com>
2025-04-11 17:01:03 +0900

    Header 클릭시 홈 화면으로 이동하도록 수정

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -1,23 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="w-full h-[60px] bg-white flex justify-center border-b border-slate-200">
       <div className="flex items-center justify-start h-full w-[1200px] px-4">
-        <Image
-          src="/images/logo.svg"
-          alt="Logo"
-          width={151}
-          height={40}
-          className="hidden sm:block"
-        />
-        <Image
-          src="/images/logo_mobile.svg"
-          alt="Logo"
-          width={71}
-          height={40}
-          className="block sm:hidden"
-        />
+        <Link href="/">
+          <Image
+            src="/images/logo.svg"
+            alt="Logo"
+            width={151}
+            height={40}
+            className="hidden sm:block"
+          />
+          <Image
+            src="/images/logo_mobile.svg"
+            alt="Logo"
+            width={71}
+            height={40}
+            className="block sm:hidden"
+          />
+        </Link>
       </div>
     </header>
   );
